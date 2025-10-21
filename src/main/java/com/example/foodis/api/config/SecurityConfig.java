905dd665166bb32196eea17c0d45ea03d6583ebe
orhan <orhantrkmn749@gmail.com>
2025-10-21 14:22:37 +0300

    last commit

--- src/main/java/com/example/foodis/api/config/SecurityConfig.java
+++ src/main/java/com/example/foodis/api/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("https://caring-passion-production-c8f2.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
